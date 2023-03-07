e635501b8dee90e34d5bca9abd674742f2c0cdbf
jxes993409 <jxes993409@gmail.com>
2023-03-07 20:07:52 +0800

    Add File

--- HW07/src/lib/readfile.cpp
+++ HW07/src/lib/readfile.cpp
@@ -4,100 +4,113 @@
 #include <ctype.h>
 #include "readfile.h"
 
-void read_string(const char input_name[], Hasharray **hash_array, Hashaddress **hash_address)
+void initialize(Hasharray *hash_array, Hashaddress *hash_address)
+{
+  for(int i = 0; i < TABLE_SIZE; i++)
+  {
+    hash_address[i].head = hash_array[i].value;
+    hash_address[i].tail = hash_array[i].value;
+  }
+}
+
+void read_string(const char input_name[], Hasharray *hash_array, Hashaddress *hash_address)
 {
   FILE *input_file = fopen(input_name, "r");
-  char *line = new char[MAX_INPUT_LENGTH];
+  char *line = new char[MAX_INPUT_LENGTH + 1];
 
   while((fscanf(input_file, "%[^\n]%*c", line)) != EOF)
   {
-    int string_sum = 0;
+    unsigned int hash_index;
+    // string_id store the number of every alphabet
+    // ex: abcc, count[a] = 1, count[b] = 1, count[c] = 2 => A = 0, B = 1, C = 2, ..., U = 20
+    // string_id = "BBCAAAAAAAAAAAAAAAAAAAAAAA"
     char string_id[MAX_ID_LENGTH];
-    memset(string_id, 0, size_t(MAX_ID_LENGTH));
-    hash_function(line, string_id, &string_sum);
-    hash_address[int(strlen(line)) - 1][string_sum].tail->next = create_node(line, string_id);
-    hash_address[int(strlen(line)) - 1][string_sum].tail = hash_address[int(strlen(line)) - 1][string_sum].tail->next;
-    
+    // clear the string_id
+    memset(string_id, 0, size_t(MAX_ID_LENGTH + 1));
+    // compute the string_id and hash_value
+    hash_index = hash_function(line, string_id);
+    hash_address[hash_index].tail->next = create_node(line, string_id); // store the last node to the tail
+    hash_address[hash_index].tail = hash_address[hash_index].tail->next; // update the tail
   }
   fclose(input_file);
   delete [] line;
 }
 
-void read_target(const char input_name[], const char output_name[], Hasharray **hash_array, Hashaddress **hash_address)
+void read_target(const char input_name[], const char output_name[], Hasharray *hash_array, Hashaddress *hash_address)
 {
   FILE *input_file = fopen(input_name, "r");
+  FILE *output_file = fopen(output_name, "a+");
   char *line = new char[MAX_INPUT_LENGTH];
 
   while((fscanf(input_file, "%[^\n]%*c", line)) != EOF)
   {
-    int string_sum = 0;
+    unsigned int hash_index;
     char string_id[MAX_ID_LENGTH];
     char *output_buffer = new char[MAX_OUTPUT_LENGTH]();
-    memset(string_id, 0, size_t(MAX_ID_LENGTH));
-
-    hash_function(line, string_id, &string_sum);
-    find_target(string_id, output_buffer, int(strlen(line) - 1), string_sum, hash_array, hash_address);
-    write_output(output_name, output_buffer);
+    memset(string_id, 0, size_t(MAX_ID_LENGTH + 1));
+    hash_index = hash_function(line, string_id);
+    find_target(string_id, output_buffer, hash_index, hash_array, hash_address);
+    fprintf(output_file, "%s", output_buffer);
     delete [] output_buffer;
   }
   fclose(input_file);
+  fclose(output_file);
   delete [] line;
 }
 
-void find_target(const char string_id[], char output_buffer[], int i, int j, Hasharray **hash_array, Hashaddress **hash_address)
+void find_target(const char string_id[], char output_buffer[], int hash_index, Hasharray *hash_array, Hashaddress *hash_address)
 {
   int flag = 0;
-  hash_array[i][j].value = hash_address[i][j].head;
-  if(hash_address[i][j].head != hash_address[i][j].tail)
+  // point the hash_array to the head
+  hash_array[hash_index].value = hash_address[hash_index].head;
+  // if head = tail, the chain is empty
+  if(hash_address[hash_index].head != hash_address[hash_index].tail)
   {
-    hash_array[i][j].value = hash_array[i][j].value->next;
+    hash_array[hash_index].value = hash_array[hash_index].value->next;
     while(true)
     {
-      if(!strcmp(string_id, hash_array[i][j].value->string_id))
+      if(strlen(string_id) == strlen(hash_array[hash_index].value->string_id)) // compare the length
       {
-        flag = 1;
-        strcat(output_buffer, hash_array[i][j].value->input_string);
-        strcat(output_buffer, " ");
+        if(!strcmp(string_id, hash_array[hash_index].value->string_id)) // compare the string_id
+        {
+          flag = 1; // find the target, set the flag
+          strcat(output_buffer, hash_array[hash_index].value->input_string); // store the string to buffer
+          strcat(output_buffer, " ");
+        }
       }
-      if(hash_array[i][j].value->next != NULL) {hash_array[i][j].value = hash_array[i][j].value->next;}
+      if(hash_array[hash_index].value->next != NULL) {hash_array[hash_index].value = hash_array[hash_index].value->next;} // chain = chain->next
       else {break;}
     }
   }
   if(flag) {strcat(output_buffer, "\n");}
-  if(!flag) {strcat(output_buffer, "0\n");}
+  if(!flag) {strcat(output_buffer, "0\n");} //if flag = 0, did not find the target, output 0
 }
 
-void hash_function(const char line[], char *string_id, int *string_sum)
+unsigned int hash_function(const char line[], char *string_id)
 {
-  int *char_count = new int[26]();
-  for(int i = 0; i < int(strlen(line)); i++)
+  int prime_table[26] = {677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 767,
+                         769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857};
+  int char_count[26] = {0};
+  long long unsigned int hash_value = 1;
+  for(size_t i = 0; i < strlen(line); i++)
   {
-    *string_sum = *string_sum + int(line[i]) - 'a';
-    char_count[line[i] - 'a']++;
+    hash_value *= prime_table[line[i] - 'a'];
+    char_count[line[i] - 'a']++; // the number of every alphabet
   }
   for(int i = 0; i < 26; i++)
   {
     char temp[1];
-    memset(temp, 0, size_t(1));
-    temp[0] = char(char_count[i] + 'A');
+    temp[0] = char(char_count[i] + 'A'); // if char_count[2] = 3 => the number of c is 3, char(3 + 'A') = D
     strcat(string_id, temp);
   }
-
-  delete [] char_count;
-}
-
-void write_output(const char output_name[], const char output_buffer[])
-{
-  FILE *output_file = fopen(output_name, "a+");
-  fprintf(output_file, "%s", output_buffer);
-  fclose(output_file);
+  return ((hash_value % (TABLE_SIZE * 2)) - 1) / 2;
 }
 
 Hashtable *create_node(const char line[], const char string_id[])
 {
   Hashtable *new_node = new Hashtable;
-  strncpy(new_node->input_string, line, strlen(line));
-  strncpy(new_node->string_id, string_id, strlen(string_id));
+  strncpy(new_node->input_string, line, strlen(line) + 1);
+  strncpy(new_node->string_id, string_id, strlen(string_id) + 1);
   new_node->next = NULL;
   return new_node;
 }
@@ -106,10 +119,10 @@ void delete_node(Hashtable *del_next, Hashtable *del)
 {
   // move the next node value to the delete one
   // delete the next node
-  memset(del->input_string, 0, size_t(MAX_INPUT_LENGTH));
-  memset(del->string_id, 0, size_t(MAX_INPUT_LENGTH));
-  strncpy(del->input_string, del_next->input_string, strlen(del_next->input_string));
-  strncpy(del->string_id, del_next->string_id, strlen(del_next->string_id));
+  memset(del->input_string, 0, size_t(MAX_INPUT_LENGTH + 1));
+  memset(del->string_id, 0, size_t(MAX_INPUT_LENGTH + 1));
+  strncpy(del->input_string, del_next->input_string, strlen(del_next->input_string) + 1);
+  strncpy(del->string_id, del_next->string_id, strlen(del_next->string_id) + 1);
   del->next = del_next->next;
   delete [] del_next->input_string;
   delete [] del_next->string_id;
--- HW07/src/lib/readfile.h
+++ HW07/src/lib/readfile.h
@@ -1,15 +1,15 @@
 #ifndef _READFILE_H_
 #define _READFILE_H_
 
+#define TABLE_SIZE 50000
 #define MAX_INPUT_LENGTH 20
-#define MAX_STRING_SUM 500
-#define MAX_ID_LENGTH 28
-#define MAX_OUTPUT_LENGTH 50000
+#define MAX_ID_LENGTH 26
+#define MAX_OUTPUT_LENGTH 25000
 
 typedef struct _hash_table
 {
-  char *input_string = new char[MAX_INPUT_LENGTH];
-  char *string_id = new char[MAX_ID_LENGTH];
+  char *input_string = new char[MAX_INPUT_LENGTH + 1];
+  char *string_id = new char[MAX_ID_LENGTH + 1];
   _hash_table *next = NULL;
 } Hashtable;
 
@@ -24,12 +24,11 @@ typedef struct _hash_address
   Hashtable *tail = new Hashtable;
 } Hashaddress;
 
-
-void read_string(const char [], Hasharray **, Hashaddress **);
-void read_target(const char [], const char [], Hasharray **, Hashaddress **);
-void find_target(const char [], char [], int, int, Hasharray **, Hashaddress **);
-void hash_function(const char [], char *, int *);
-void write_output(const char[], const char[]);
+void initialize(Hasharray *, Hashaddress *);
+void read_string(const char [], Hasharray *, Hashaddress *);
+void read_target(const char [], const char [], Hasharray *, Hashaddress *);
+void find_target(const char [], char [], int, Hasharray *, Hashaddress *);
+unsigned int hash_function(const char [], char *);
 Hashtable *create_node(const char [], const char []);
 void delete_node(Hashtable *, Hashtable *);
 
